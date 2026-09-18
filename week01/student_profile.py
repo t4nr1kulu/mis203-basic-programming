9ac313f19254eccf612de4987b05be86699d8d46
t4nr1kulu <t4nr1kullu@gmail.com>
2026-09-18 22:29:45 +0300

    Create student_profile.py

---
+++ week01/student_profile.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+department = input("Enter your department: ")
+age = input("Enter your age: ")
+career_goal = input("Enter your career goal: ")
+
+print("\n--- Student Profile ---")
+print(f"Name: {name}")
+print(f"Department: {department}")
+print(f"Age: {age}")
+print(f"Career Goal: {career_goal}")
